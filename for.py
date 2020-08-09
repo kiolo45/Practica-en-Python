53310c1def572b5d5ce1e93f9ec1541c4d9d5cfa
Juan Esteban <64446257+kiolo45@users.noreply.github.com>
2020-08-08 19:21:18 -0500

    aprendiendo el ciclo for

---
+++ for.py
@@ -0,0 +1,23 @@
+# while
+# def mientras():
+#     contador = 1
+#     print(contador)
+#     while contador < 1000:
+#         contador += 1
+#         print(contador)
+
+# mientras()
+
+
+
+def main():
+    for contador in range(1, 1001):
+        print(contador)
+
+
+
+
+if __name__=='__main__':
+    main()
+
+
