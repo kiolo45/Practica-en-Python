f25382e7674562405ed70bd51774ecf8e44dba6b
Juan Esteban <64446257+kiolo45@users.noreply.github.com>
2020-08-08 19:25:14 -0500

    recorrer string con ciclo for

---
+++ recorrer_string.py
@@ -0,0 +1,15 @@
+def main():
+    # nombre = input('Escribe tu nombre: ')
+    # for letra in nombre:
+    #     print(letra)
+
+
+    frase = input('Escriebe una frase: ')
+    for letra in frase:
+        print(letra.upper())
+
+
+
+
+if __name__=='__main__':
+    main()
