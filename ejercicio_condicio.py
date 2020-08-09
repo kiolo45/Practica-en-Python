a5480e6833a60adf2b4c104d9203ee414729388c
Juan Esteban <64446257+kiolo45@users.noreply.github.com>
2020-08-08 19:26:23 -0500

    ejercicios comunes

---
+++ ejercicio_condicio.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# definimos la variable numero
+numero = int(input("Escribe un numero: "))
+
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")        
+
+    
---
+++ tuplas.py
@@ -0,0 +1,5 @@
+# tuplas
+# las tuplas son inmutables
+
+mi_tupla = (1,2,3,4,5)
+print(mi_tupla)
