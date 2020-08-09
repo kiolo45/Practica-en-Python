d4d32783523cf18a4fb26eee5e6470f2590fd06f
Juan Esteban <64446257+kiolo45@users.noreply.github.com>
2020-08-08 19:16:10 -0500

    aprendiendo condicionales if else

---
+++ condicionales.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# definimos la variable edad
+edad = int(input("Escribe tu  edad: " ))
+
+# condiciones
+if edad > 17:
+    print("Eres mayor de edad!")
+else:
+    print("Eres menor de edad :(")
+
